91f8a0116c3ab897200d62cc1b6af85886329609
Samantha Yeargin <slyeargin@gmail.com>
2014-11-22 19:34:37 -0600

    Fixed client-template and traceur path

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var http = require('http');
 var connect = require('connect');
+var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
+var logger = require('morgan')
 var directory = 'public';
 var port = process.env.PORT;
 
+
 var app = connect()
-  .use(connect.logger(':remote-addr -> :method :url [:status]'))
-  .use(connect.static(directory));
+
+  .use(logger(':remote-addr -> :method :url [:status]'))
+  .use(serveStatic(directory))
+  .use(bodyParser.urlencoded({
+      extended: true
+  }))
+  .use(bodyParser.json())
+  .use(function(req, res){
+    res.end(JSON.stringify(req.body));
+  })
 
 http.createServer(app).listen(port, function(){
   console.log('Node server listening. Port: ' + port + ', Directory: ' + directory);
