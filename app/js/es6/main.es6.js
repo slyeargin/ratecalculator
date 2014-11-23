fc2512e4d61cb5b7a65bfd40e34f52f0367e5815
Samantha Yeargin <slyeargin@gmail.com>
2014-11-23 13:25:47 -0600

    Selected option values can increase or decrease the total hourly rate within given constraints.

--- app/js/es6/main.es6.js
+++ app/js/es6/main.es6.js
@@ -4,7 +4,37 @@
   $(document).ready(init);
 
   function init(){
+    $('.hourlyRate').text($('input[name=baseRate]').val());
     $('#addOption').click(addOption);
+    $('#options').on('click', '.selectOption', adjustRate);
+  }
+
+  function adjustRate(){
+    var adjustedRate = parseInt($('input[name=baseRate]').val());
+    var selectedOptions = $('.selectOption:checked').closest('tr');
+    selectedOptions.map((i,d)=>$(d).attr('data-amount')).each((i, value)=>{
+      value = parseInt(value);
+      if (value > 0){
+        adjustedRate = increaseRate(adjustedRate, value);
+      } else {
+        adjustedRate = decreaseRate(adjustedRate, value);
+      }
+      updateRateDisplay(adjustedRate);
+    });
+  }
+
+  function updateRateDisplay(rate){
+    $('.hourlyRate').text(rate);
+  }
+
+  function increaseRate(adjustedRate, value){
+    adjustedRate += value;
+    return adjustedRate;
+  }
+
+  function decreaseRate(adjustedRate, value){
+    adjustedRate = (adjustedRate + value < 0) ? 0 : adjustedRate + value;
+    return adjustedRate;
   }
 
   function addOption(){
@@ -21,8 +51,8 @@
     $('#options tr .amount').last().text('$' + amount);
     if (addOrSubtract === 'decrease'){
       $('#options tr .amount').last().prepend('-');
+      amount *= -1;
     }
-    $('#options tr').last().attr('data-operation', addOrSubtract);
     $('#options tr').last().attr('data-amount', amount);
     clearAddOptionForm();
   }
